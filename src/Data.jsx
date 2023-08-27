c9d0366f7381cbdb962243fe59fc9c0b76844d11
afaqalii <afaqali4244@gmai.com>
2023-08-27 12:16:42 +0500

    Started building an online store

---
+++ src/Data.jsx
@@ -0,0 +1,84 @@
+const CasualData = [
+    {
+        id:1,
+        img:"https://lirp.cdn-website.com/md/dmtmpl/f67fc932-23…opt/asian_woman_is_wearing_blue_costume-1280w.jpg",
+        imgHover:"https://irp.cdn-website.com/md/dmtmpl/f67fc932-23e9-4c99-a08c-01ab694e3064/dms3rep/multi/woman_is_wearing_blue_costume.jpg",
+        title:"blue cutton costume",
+        price:"$350.00"
+    },
+    {
+        id:2,
+        img:"https://irp.cdn-website.com/md/dmtmpl/f67fc932-23e9-4c99-a08c-01ab694e3064/dms3rep/multi/Asian_woman_wears_beige_trench.jpg",
+        imgHover:"https://irp.cdn-website.com/md/dmtmpl/f67fc932-23e9-4c99-a08c-01ab694e3064/dms3rep/multi/Asian_woman_wears_beige_trench_2.jpg",
+        title:"trench coat",
+        price:"$250.00"
+    },
+    {
+        id:3,
+        img:"https://irp.cdn-website.com/md/dmtmpl/f67fc932-23e9-4c99-a08c-01ab694e3064/dms3rep/multi/afroamerican_woman_in_white_costume__sit_on_white_floor.jpg",
+        imgHover:"https://irp.cdn-website.com/md/dmtmpl/f67fc932-23e9-4c99-a08c-01ab694e3064/dms3rep/multi/afroamerican_woman_in_white_costume.jpg",
+        title:"white jacket",
+        price:"$150.00"
+    },
+    {
+        id:4,
+        img:"https://irp.cdn-website.com/md/dmtmpl/f67fc932-23e9-4c99-a08c-01ab694e3064/dms3rep/multi/two_women_in_pink_and_black_costumes.jpg",
+        imgHover:"https://irp.cdn-website.com/md/dmtmpl/f67fc932-23e9-4c99-a08c-01ab694e3064/dms3rep/multi/woman_in_pink_costume_sitting_on_chair.jpg",
+        title:"pink costume",
+        price:"$300.00"
+    },
+    {
+        id:5,
+        img:"https://irp.cdn-website.com/md/dmtmpl/f67fc932-23e9-4c99-a08c-01ab694e3064/dms3rep/multi/ginger_woman_in_mint_costume_holding_flowers.jpg",
+        imgHover:"https://irp.cdn-website.com/md/dmtmpl/f67fc932-23e9-4c99-a08c-01ab694e3064/dms3rep/multi/ginger_woman_in_mint_costume.jpg",
+        title:"mint jacket",
+        price:"$200.00"
+    },
+    {
+        id:6,
+        img:"https://irp.cdn-website.com/md/dmtmpl/f67fc932-23e9-4c99-a08c-01ab694e3064/dms3rep/multi/woman_is_hiding_her_face_with_green_sewater.jpg",
+        imgHover:"https://lirp.cdn-website.com/md/dmtmpl/f67fc932-23e9-4c99-a08c-01ab694e3064/dms3rep/multi/opt/woman_is_wearing_green_sewater-1920w.jpg",
+        title:"sweater",
+        price:"$89.00"
+    },
+    {
+        id:7,
+        img:"https://irp.cdn-website.com/md/dmtmpl/f67fc932-23e9-4c99-a08c-01ab694e3064/dms3rep/multi/plus_size_model_wears_purple_jacket_looking_right-8165d621.jpg",
+        imgHover:"https://irp.cdn-website.com/md/dmtmpl/f67fc932-23e9-4c99-a08c-01ab694e3064/dms3rep/multi/plus_size_model_wears_purple_jacket.jpg",
+        title:"sweater",
+        price:"$89.00"
+    },
+    {
+        id:8,
+        img:"https://irp.cdn-website.com/md/dmtmpl/f67fc932-23e9-4c99-a08c-01ab694e3064/dms3rep/multi/woman_holding_white_jacket.jpg",
+        imgHover:"https://irp.cdn-website.com/md/dmtmpl/f67fc932-23e9-4c99-a08c-01ab694e3064/dms3rep/multi/woman_in_white_costume_sitting_on_chair.jpg",
+        title:"white costume",
+        price:"$239.00"
+    },
+    {
+        id:9,
+        img:"https://irp.cdn-website.com/md/dmtmpl/f67fc932-23e9-4c99-a08c-01ab694e3064/dms3rep/multi/blond_woman_wearing_pink_costume_and_white_tshirt.jpg",
+        imgHover:"https://irp.cdn-website.com/md/dmtmpl/f67fc932-23e9-4c99-a08c-01ab694e3064/dms3rep/multi/pink_costume_and_white_tshirt.jpg",
+        title:"viscose pink costume",
+        price:"$339.00"
+    },
+    {
+        id:10,
+        img:"https://irp.cdn-website.com/md/dmtmpl/f67fc932-23e9-4c99-a08c-01ab694e3064/dms3rep/multi/blue_jeans_and_heeled_shoes.jpg",
+        imgHover:"https://irp.cdn-website.com/md/dmtmpl/f67fc932-23e9-4c99-a08c-01ab694e3064/dms3rep/multi/Woman_wears_blue_jeans_and_heeled_shoes.jpg",
+        title:"straigth jeans",
+        price:"$530.00"
+    },
+    {
+        id:11,
+        img:"https://irp.cdn-website.com/md/dmtmpl/f67fc932-23e9-4c99-a08c-01ab694e3064/dms3rep/multi/blond_woman_wears_pink_tshirt.jpg",
+        imgHover:"https://irp.cdn-website.com/md/dmtmpl/f67fc932-23e9-4c99-a08c-01ab694e3064/dms3rep/multi/blond_woman_wears_pink_tshirt_2.jpg",
+        title:"pink t-shirt",
+        price:"$30.00"
+    },
+]
+
+
+
+
+
